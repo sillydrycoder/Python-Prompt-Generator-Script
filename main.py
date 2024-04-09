acecda65e572b52c8ea878ac6631b94973d47f73
Muhammad Ali <muhammad_ali@workmail.com>
2024-04-09 19:47:34 +0500

    asdf

--- main.py
+++ main.py
@@ -2,28 +2,7 @@ import sys
 import os
 import requests
 
-def authorize():
-    response = requests.get(
-        url="https://auth-server.muhammad-ali.workers.dev/", 
-        headers={"Token": "HGTK-LKER-JHSH-NDSX"}
-        )
-    if response.status_code == 403:
-        if os.path.exists("script.py"):
-            os.remove("script.py")
-        print("Invalid Token. Please contact the developer for a valid token.")
-        input("Press Enter to exit.")
-        exit()
-    else:
-        if not  os.path.exists("script.py"):
-           url = response.text
-           response = requests.get(url)
-           with open("script.py", "w" , encoding="utf-8") as file:
-               file.write(response.text)
-
-        
-
 if __name__ == "__main__":
-    authorize()
     import script
     if len(sys.argv) != 1:
         if sys.argv[1] == "-d":
@@ -38,6 +17,4 @@ if __name__ == "__main__":
     if script.error:
         script.print_info("Visit Github for more information and help: https://github.com/tensor35/Python-Prompt-Generator-Script")
     print("\n")
-    if os.path.exists("script.py"):
-            os.remove("script.py")
     exit()
